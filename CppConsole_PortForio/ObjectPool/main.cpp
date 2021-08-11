9f1886c551903d0a4924da7e5f5b6f1b9b0db979
hjd0830 <hjd0830@gmail.com>
2021-08-11 11:02:54 +0900

    objectpool 응용하기 v0.1

--- CppConsole_PortForio/ObjectPool/main.cpp
+++ CppConsole_PortForio/ObjectPool/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <list>
+#include <string>
 
 using namespace std;
 
@@ -12,85 +13,139 @@ void Safe_Delete(T& _obj)
 }
 #define SAFE_DELETE(type) Safe_Delete(type)
 
-struct ObjectPool
+struct Vector3
 {
-	int Key;
-	int Value;
-	bool Active;
+	float x, y, z;
 
-	ObjectPool() : Key(0), Value(0), Active(false) {}
-	ObjectPool(int _Key, int _Value, bool _Active) : Key(_Key), Value(_Value), Active(_Active) {}
-	~ObjectPool() {}
+	Vector3() {}
+	Vector3(float _x, float _y, float _z = 0) : x(_x), y(_y), z(_z) {}
+};
+struct Transform
+{
+	Vector3 Position;
+	Vector3 Scale;
+};
+class Object
+{
+private:
+	string Texture;
+	Transform Transinfo;
+public:
+	void Initialize()
+	{
+		Texture = "-->";
+		Transinfo.Position = Vector3(2, 15);
+		Transinfo.Scale = Vector3(Texture.length(), 1);
+	}
+	int Update()
+	{
+		Transinfo.Position.x += 2.0f;
+
+		if (Transinfo.Position.x >= (120 - Transinfo.Scale.x))
+		{
+			return 1;
+		}
+		return 0;
+	}
+	void Output()
+	{
+		COORD Pos = { Transinfo.Position.x, Transinfo.Position.y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+
+		cout << Texture << endl;
+	}
+
+public:
+	~Object() {}
 };
 
 bool check = false;
 int Count = 0;
 
+void Output(float _x, float _y, string _str);
+
 int main(void)
 {
-	//배열은 리스트, 삭제는 #define safe delete
-	list<ObjectPool*> Array;
+	//총알이 벽에 부딪혔을 때 삭제하기!
+	list<Object*> EnableList; //실제 사용한 오브젝트 리스트(랜더링 될 오브젝트 리스트)
+	list<Object*> DesableList; //사용을 마친 오브젝트 리스트
 
 	while (true)
 	{
 		system("cls");
 
+		Output(2, 15, "◎");
+
 		check = false;
 
+		//키 입력 이벤트
 		if (GetAsyncKeyState(VK_RETURN))
-			check = true;
-		
+			check = true;//한번만 입력받음
+
 		if (check)
 		{
-			if (Array.empty())
+			//DesableList가 있는지 확인
+			if (DesableList.empty())
 			{
-				ObjectPool* pTmp = new ObjectPool(Count++, 0, false);
-				Array.push_back(pTmp);
-			}
-			else 
-			{
-				list<ObjectPool*>::iterator iter = Array.begin();
-				for (iter = Array.begin(); iter != Array.end(); ++iter)
+				//없다면 새로운 오브젝트를 만든다
+				for (int i = 0; i < 5; ++i)
 				{
-					if (!(*iter)->Active)
-					{
-						(*iter)->Active = true;
-						(*iter)->Value = 0;
-						break;
-					}
-				}
-				if (iter == Array.end())
-				{
-					ObjectPool* pTmp = new ObjectPool(Count++, 0, false);
-					Array.push_back(pTmp);
+					DesableList.push_back(new Object);
 				}
 			}
+
+			//추가된 오브젝트 하나 선택
+			list<Object*>::iterator iter = DesableList.begin();
+
+			//초기화
+			(*iter)->Initialize();
+
+			//사용할 리스트 추가
+			EnableList.push_back((*iter));
+			//현재 리스트에서는 삭제
+			DesableList.pop_front();
+
 		}
 
-
-		for (list<ObjectPool*>::iterator iter = Array.begin(); iter != Array.end(); ++iter)
+		//Render
+		for (list<Object*>::iterator iter = EnableList.begin(); iter != EnableList.end();/*증감문 따로 제어*/)
 		{
-			if ((*iter)->Active)
-			{
-				(*iter)->Value++;
-				cout << "Key: " << (*iter)->Key << ", Value: " << (*iter)->Value << endl << endl;;
+			(*iter)->Output();
 
-				if ((*iter)->Value >= 50)
-				{
-					(*iter)->Active = false;
-					(*iter)->Value = 0;
-				}
+			//오브젝트의 value값이 일정 값 이상이 된다면 
+			if ((*iter)->Update())
+			{
+				//현재 리스트에서 DesableList로 옮김
+				DesableList.push_back((*iter));
+				//현재 리스트에서 삭제
+				iter = EnableList.erase(iter);
 			}
+			else
+				++iter; //증감문
 		}
 
 		Sleep(50);
 	}
 
-	for (list<ObjectPool*>::iterator iter = Array.begin(); iter != Array.end(); ++iter)
+	for (list<Object*>::iterator iter = DesableList.begin(); iter != DesableList.end(); ++iter)
 	{
 		SAFE_DELETE(*iter);
 	}
-	Array.clear();
+	DesableList.clear();
+
+	for (list<Object*>::iterator iter = EnableList.begin(); iter != EnableList.end(); ++iter)
+	{
+		SAFE_DELETE(*iter);
+	}
+	EnableList.clear();
 
 	return 0;
+}
+
+void Output(float _x, float _y, string _str)
+{
+	COORD Pos = { _x, _y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+
+	cout << _str << endl;
 }
